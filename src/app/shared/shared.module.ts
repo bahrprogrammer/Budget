bc51c4ea6c96a3f4c70eaed57cfd0354539c2c10
jeffreyBahr <bahr.programmer@gmail.com>
2020-03-30 18:18:00 -0600

    updated angular materials, jasmine, and karma

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
-import { MatButtonModule, MatDatepickerModule, MatInputModule, MatNativeDateModule, MatTooltipModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
