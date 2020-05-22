86b6f428cfbb80bc3ced38a7c2c10a70fbd564e4
jeffreyBahr <bahr.programmer@gmail.com>
2020-05-22 17:36:29 -0600

    removing mock data

--- src/app/budget/budget.service.ts
+++ src/app/budget/budget.service.ts
@@ -11,19 +11,9 @@ export class BudgetService {
   currentMonth: Calendar;
   nextMonth: Calendar;
 
-  expenseList: IBudgetItem[] = [
-    { id: 1, date: new Date('5/1/2020'), source: 'rent', amount: 1000.47 },
-    { id: 2, date: new Date('5/11/2020'), source: 'phone', amount: 70.74 },
-    { id: 3, date: new Date('2020-05-24'), source: 'car', amount: 289.23 },
-    { id: 4, date: new Date('5/1/2020'), source: 'internet', amount: 81.95 }
-  ];
+  expenseList: IBudgetItem[] = [];
 
-  incomeList: IBudgetItem[] = [
-    { id: 1, date: new Date('5/1/2020'), source: 'paycheck', amount: 1500 },
-    { id: 2, date: new Date('5/15/2020'), source: 'paycheck', amount: 1500 },
-    { id: 1, date: new Date('6/1/2020'), source: 'paycheck', amount: 1500 },
-    { id: 2, date: new Date('6/15/2020'), source: 'paycheck', amount: 1500 }
-  ];
+  incomeList: IBudgetItem[] = [];
 
   constructor() {
     const date: Date = new Date();
