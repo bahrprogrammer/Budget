6a85d34eaba707d8281c6c85820d56bfb7282a26
jeffreyBahr <bahr.programmer@gmail.com>
2020-06-25 16:07:33 -0600

    adding radix parameter

--- src/app/budget/budget.service.ts
+++ src/app/budget/budget.service.ts
@@ -36,7 +36,7 @@ export class BudgetService {
 
   getItemMonth(item: IBudgetItem) {
     const monthString = (item.date.toString()).substr(5, 2);
-    const itemMonth = parseInt(monthString) - 1;
+    const itemMonth = parseInt(monthString, 16) - 1;
 
     return itemMonth;
   }
