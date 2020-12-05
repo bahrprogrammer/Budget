98da67b1a7cd02a142338693632f15f3a784eb97
JeffreyBahr <bahr.programmer@gmail.com>
2020-12-04 17:42:41 -0700

    firebase analytics setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AngularFireAnalytics, AngularFireAnalyticsModule, ScreenTrackingService } from '@angular/fire/analytics';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent } from './about/about.component';
 import { BudgetModule } from './budget/budget.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,11 +22,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalytics,
+    AngularFireAnalyticsModule,
     BudgetModule,
     BrowserAnimationsModule,
     NgbModule
   ],
-  providers: [],
+  providers: [ScreenTrackingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBmHEsSishxKakriRo7ylqK3RrVM6JtQsM',
+    authDomain: 'freebudgetcalendar.firebaseapp.com',
+    databaseURL: 'https://freebudgetcalendar.firebaseio.com',
+    projectId: 'freebudgetcalendar',
+    storageBucket: 'freebudgetcalendar.appspot.com',
+    messagingSenderId: '538512904419',
+    appId: '1:538512904419:web:51131a60c4348e2f7b75d6',
+    measurementId: 'G-FTL8P142JK'
+  }
 };
 
 /*
